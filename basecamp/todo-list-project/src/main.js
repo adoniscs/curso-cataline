2b1aa622e008aa22965f2f364a93e487773f9e6b
Adonis Cipriano <desenvolvimento777@gmail.com>
2022-01-04 19:51:04 -0300

    render com js puro - comentado

--- basecamp/todo-list-project/src/main.js
+++ basecamp/todo-list-project/src/main.js
@@ -3,10 +3,66 @@ import './assets/css/main.css'
 
 const apiTodos = new Todos()
 
+
+/* <li class="todo">
+  <label class="checkbox-input">
+    <input type="checkbox" checked="true" />
+    <span class="checkmark"></span>
+  </label>
+  <span class="todo-text">Lavar Roupa</span>
+  <a class="todo-delete"></a>
+</li> */
+
+
+// ======== RENDERIZAR OS ITENS COM JS PURO ====================
+/*
 async function exec() {
   const todos = await apiTodos.index()
 
-  console.log(todos)
+  const ul = document.querySelector('ul.todos')
+
+  // obtendo os todos
+  todos.forEach((todo) => {
+    // todo / li
+    const li = document.createElement('li')
+    li.setAttribute('class', 'todo')
+
+    // todo checkbox
+    const todoCheckbox = document.createElement('label')
+    todoCheckbox.setAttribute('class', 'checkbox-input')
+
+    // todo checkbox -> input
+    const inputCheckbox = document.createElement('input')
+    inputCheckbox.setAttribute('type', 'checkbox')
+    if (todo.done) {
+      inputCheckbox.setAttribute('checked', 'checked')
+    }
+
+    // todo checkbox -> span
+    const spanCheckbox = document.createElement('span')
+    spanCheckbox.setAttribute('class', 'checkmark')
+
+    todoCheckbox.appendChild(inputCheckbox)
+    todoCheckbox.appendChild(spanCheckbox)
+
+    // todo text
+    const todoText = document.createElement('span')
+    todoText.setAttribute('class', 'todo-text')
+
+    const spanText = document.createTextNode(todo.text)
+    todoText.appendChild(spanText)
+
+    // todo delete
+    const todoDelete = document.createElement('span')
+    todoDelete.setAttribute('class', 'todo-delete')
+
+    li.appendChild(todoCheckbox)
+    li.appendChild(todoText)
+    li.appendChild(todoDelete)
+
+    ul.appendChild(li)
+  })
 }
 
 exec()
+*/
