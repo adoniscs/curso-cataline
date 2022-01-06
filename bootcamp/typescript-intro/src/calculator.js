658a74556fe0c9e80a12056c1c12f87f79f455bb
Adonis Cipriano <desenvolvimento777@gmail.com>
2022-01-05 23:52:17 -0300

    introducao ao typescript finalziada

---
+++ bootcamp/typescript-intro/src/calculator.js
@@ -0,0 +1,9 @@
+export const sum = (a, b) => {
+  return `O resultado da soma de ${a} + ${b} é igual a ${Number(a) + Number(b)}`
+}
+
+export const diference = (a, b) => {
+  return `O resultado da diferença de ${a} - ${b} é igual a ${
+    Number(a) - Number(b)
+  }`
+}
--- bootcamp/typescript-intro/src/main.ts
+++ bootcamp/typescript-intro/src/main.ts
@@ -1,1 +1,27 @@
-console.log('olá, mundo!')
+interface User {
+  name: string
+  address?: {
+    city: string
+    UF: string
+  }
+}
+
+function showCity(user: User) {
+  return user
+}
+
+const response = showCity({
+  name: 'Adonis',
+  address: {
+    city: 'São Paulo',
+    UF: 'SP',
+  },
+})
+
+console.log(response)
+
+import { sum } from './calculator'
+import { diference } from './calculator'
+
+console.log(sum('50', '19'))
+console.log(diference(50, 19))
