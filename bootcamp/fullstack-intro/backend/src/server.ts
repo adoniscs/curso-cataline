07562db18291c9451224bbc0478409410157d9dd
Adonis Cipriano <desenvolvimento777@gmail.com>
2022-01-06 02:14:50 -0300

    Revert "rotas do servidor configuradas"
    This reverts commit 3b7de3ae529eea3dcd19bcb57487bcbe6fa8ecbb.

--- bootcamp/fullstack-intro/backend/src/server.ts
+++ bootcamp/fullstack-intro/backend/src/server.ts
@@ -1,21 +1,5 @@
-import express from 'express'
+import express from "express";
 const server = express()
-
-server.get('/users', (request, response) => {
-  return response.json(['usuário 1', 'usuário 2'])
-})
-
-server.post('/users', (request, response) => {
-  return response.json({ message: 'Criando usuário' })
-})
-
-server.put('/users/:id', (request, response) => {
-  return response.json({ message: 'Atualizando usuário' })
-})
-
-server.delete('/users/:id', (request, response) => {
-  return response.json({ message: 'Deletando usuário ' })
-})
 
 server.listen(3000, () => {
   console.log('Server is running!')
